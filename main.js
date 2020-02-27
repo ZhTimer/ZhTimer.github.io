0c51e048074b984e4b39c61a59abf2d81af107d2
darkmidus <39868305+darkmidus@users.noreply.github.com>
2020-02-26 19:54:17 -0600

    Tryna Fix js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //	Simple example of using private variables 
 //	To start the stopwatch:
 //		obj.start();
@@ -14,104 +15,114 @@
 //	To reset a paused stopwatch
 //		obj.stop();
 //
-var	clsStopwatch = function() {
-		// Private vars
-		var	startAt	= 0;	// Time of last start / resume. (0 if not running)
-		var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
+var clsStopwatch = function () {
+  // Private vars
+  var startAt = 0; // Time of last start / resume. (0 if not running)
+  var lapTime = 0; // Time on the clock when last stopped in milliseconds
 
-		var	now	= function() {
-				return (new Date()).getTime(); 
-			}; 
- 
-		// Public methods
-		// Start or resume
-		this.start = function() {
-				startAt	= startAt ? startAt : now();
-			};
+  var now = function () {
+    return new Date().getTime();
+  };
 
-		// Stop or pause
-		this.stop = function() {
-				// If running, update elapsed time otherwise keep it
-				lapTime	= startAt ? lapTime + now() - startAt : lapTime;
-				startAt	= 0; // Paused
-			};
+  // Public methods
+  // Start or resume
+  this.start = function () {
+    startAt = startAt ? startAt : now();
+  };
 
-		// Reset
-		this.reset = function() {
-				lapTime = startAt = 0;
-			};
+  // Stop or pause
+  this.stop = function () {
+    // If running, update elapsed time otherwise keep it
+    lapTime = startAt ? lapTime + now() - startAt : lapTime;
+    startAt = 0; // Paused
+  };
 
-		// Duration
-		this.time = function() {
-				return lapTime + (startAt ? now() - startAt : 0); 
-			};
-	};
+  // Reset
+  this.reset = function () {
+    lapTime = startAt = 0;
+  };
+
+  // Duration
+  this.time = function () {
+    return lapTime + (startAt ? now() - startAt : 0);
+  };
+};
 
 var x = new clsStopwatch();
 var $time;
 var clocktimer;
 
 function pad(num, size) {
-	var s = "0000" + num;
-	return s.substr(s.length - size);
+  var s = "0000" + num;
+  return s.substr(s.length - size);
 }
 
 function formatTime(time) {
-	var h = m = s = ms = 0;
-	var newTime = '';
+  var h = m = s = ms = 0;
+  var newTime = '';
 
-	h = Math.floor( time / (60 * 60 * 1000) );
-	time = time % (60 * 60 * 1000);
-	m = Math.floor( time / (60 * 1000) );
-	time = time % (60 * 1000);
-	s = Math.floor( time / 1000 );
-	ms = time % 1000;
+  h = Math.floor(time / (60 * 60 * 1000));
+  time = time % (60 * 60 * 1000);
+  m = Math.floor(time / (60 * 1000));
+  time = time % (60 * 1000);
+  s = Math.floor(time / 1000);
+  ms = time % 1000;
 
-	newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
-	return newTime;
+  newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+  return newTime;
 }
 
 function show() {
-	$time = document.getElementById('time');
-	update();
+  $time = document.getElementById('time');
+  update();
 }
 
 function update() {
-	$time.innerHTML = formatTime(x.time());
+  $time.innerHTML = formatTime(x.time());
 }
 
 function start() {
-	clocktimer = setInterval("update()", 1);
-	x.start();
+  clocktimer = setInterval("update()", 1);
+  x.start();
+  playAudio();
 }
 
 function stop() {
-	x.stop();
-	clearInterval(clocktimer);
+  x.stop();
+  clearInterval(clocktimer);
 }
 
 function reset() {
-	stop();
-	x.reset();
-	update();
+  stop();
+  x.reset();
+  update();
+}
+// Play Audio
+var y = document.getElementById("myAudio");
+
+function playAudio() {
+  y.play();
 }
 
+function pauseAudio() {
+  y.pause();
+}
 // Keypress Script
 var pressed = true;
-document.body.onkeyup = function(e) {
-    if(e.keyCode == 32){
-      if(pressed) {
-        setTimeout(start, 0)
-        pressed = false;
-      } else { 
-        stop();
-        pressed = true; 
-      }
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    if (pressed) {
+      setTimeout(start, 3000);
+      pressed = false;
+    } else {
+      stop();
+      pressed = true;
     }
-    if(e.keyCode == 82) {
-      reset()
-     }
-}
+  }
+  if (e.keyCode == 82) {
+    reset();
+  }
+};
 
 function darkmode() {
   var element = document.body;
